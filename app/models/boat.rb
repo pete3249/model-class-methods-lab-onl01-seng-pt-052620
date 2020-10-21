f4cdb88a5dce5f0553f14f5f9eafb117dbac4c07
pete3249 <katelynrpeterson16@gmail.com>
2020-10-21 14:31:20 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -16,15 +16,15 @@ class Boat < ActiveRecord::Base
   end
 
   def self.last_three_alphabetically
-    # all.order(name: :desc).limit(3)
+    all.order(name: :desc).limit(3)
   end
 
   def self.without_a_captain
-    # where(captain_id: nil)
+    where(captain_id: nil)
   end
 
   def self.sailboats
-    # includes(:classifications).where(classifications: { name: 'Sailboat' })
+    includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
   def self.with_three_classifications
@@ -32,14 +32,14 @@ class Boat < ActiveRecord::Base
     # regularly. Just know that we can get this out of the database in
     # milliseconds whereas it would take whole seconds for Ruby to do the same.
     #
-    # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
   def self.non_sailboats
-    # where("id NOT IN (?)", self.sailboats.pluck(:id))
+    where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
 
   def self.longest
-    # order('length DESC').first
+    order('length DESC').first
   end
 end
